19e7eedae1f3fa4945addd211c8457dc0bd40df1
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-11 04:22:48 +1000

    Remove some of the pointless but numerous differences from upstream

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -11,8 +11,8 @@ except ImportError:
     from distutils.core import setup
 
 
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist upload")
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
     sys.exit()
 
 
@@ -24,47 +24,47 @@ class PyTest(TestCommand):
 
     def run_tests(self):
         #import here, cause outside the
-        #eggs aren"t loaded
+        #eggs aren't loaded
         import pytest
         errno = pytest.main(self.test_args)
         sys.exit(errno)
 
 
-readme = open("README.rst").read()
-history = open("HISTORY.rst").read().replace(".. :changelog:", '')
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
-    name="{{ cookiecutter.repo_name }}",
+    name='{{ cookiecutter.repo_name }}',
     version={{ cookiecutter.repo_name }}.__version__,
-    description="{{ cookiecutter.project_short_description }}",
-    long_description=readme + "\n\n" + history,
-    author="{{ cookiecutter.full_name }}",
-    author_email="{{ cookiecutter.email }}",
-    url="https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}",
+    description='{{ cookiecutter.project_short_description }}',
+    long_description=readme + '\n\n' + history,
+    author='{{ cookiecutter.full_name }}',
+    author_email='{{ cookiecutter.email }}',
+    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
     packages=[
-        "{{ cookiecutter.repo_name }}",
+        '{{ cookiecutter.repo_name }}',
     ],
-    package_dir={"{{ cookiecutter.repo_name }}": "{{ cookiecutter.repo_name }}"},
+    package_dir={'{{ cookiecutter.repo_name }}': '{{ cookiecutter.repo_name }}'},
     include_package_data=True,
     install_requires=[
     ],
     setup_requires=[
-        "flake8",
+        'flake8',
     ],
-    license="BSD",
+    license='BSD',
     zip_safe=False,
-    keywords="{{ cookiecutter.repo_name }}",
+    keywords='{{ cookiecutter.repo_name }}',
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
     ],
-    tests_require=["pytest>=2.3.5"],
-    cmdclass = {"test": PyTest},
+    tests_require=['pytest>=2.3.5'],
+    cmdclass = {'test': PyTest},
 )
