8b99a176667c1a65c732a8df56b035c918b25067
Audrey Roy <audreyr@gmail.com>
2013-08-17 00:59:23 +0200

    Remove unnecessary os and shutil imports.

--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
@@ -8,8 +8,6 @@ test_{{ cookiecutter.repo_name }}
 Tests for `{{ cookiecutter.repo_name }}` module.
 """
 
-import os
-import shutil
 import unittest
 
 from {{ cookiecutter.repo_name }} import {{ cookiecutter.repo_name }}
