b321afc4505f06ad6abc2bc65439ee04035c41dc
Michael Joseph <michaeljoseph+github@gmail.com>
2013-10-10 03:06:25 +0200

    Pared setup

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,50 +1,19 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import os
-import sys
-
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
-
-readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+from setuptools import setup
+import {{ cookiecutter.repo_name }}
 
 setup(
-    name='{{ cookiecutter.repo_name }}',
-    version='{{ cookiecutter.version }}',
-    description='{{ cookiecutter.project_short_description }}',
-    long_description=readme + '\n\n' + history,
-    author='{{ cookiecutter.full_name }}',
+    name={{ cookiecutter.repo_name }}.__name__,
+    version={{ cookiecutter.repo_name }}.__version__,
+    description'{{ cookiecutter.project_short_description }}',
+    author='{{ cookiecutter.name }}',
     author_email='{{ cookiecutter.email }}',
-    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
-    packages=[
-        '{{ cookiecutter.repo_name }}',
-    ],
+    url={{ cookiecutter.repo_name }}.__url__
+    packages=[{{ cookiecutter.repo_name }}.__name__],
     package_dir={'{{ cookiecutter.repo_name }}': '{{ cookiecutter.repo_name }}'},
     include_package_data=True,
     install_requires=[
+	
     ],
-    license="BSD",
-    zip_safe=False,
-    keywords='{{ cookiecutter.repo_name }}',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-    ],
-    test_suite='tests',
+    test_suite='nose.collector',
+    license=open('LICENSE').read(),
 )
