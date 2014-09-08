74480f80fe7e3f5398bf0aeae6b09d006a5debfc
Simeon Visser <svisser@users.noreply.github.com>
2014-09-09 00:12:54 +0100

    Ensure file handles are closed using with statement

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -8,8 +8,11 @@ except ImportError:
     from distutils.core import setup
 
 
-readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
     # TODO: put package requirements here
