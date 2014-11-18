597a4a34c23f9340dabb7a190879a4aa0e443dc4
Juan José Conti <jjconti@gmail.com>
2014-11-18 00:56:48 -0300

    Use double quote to avoid broken strings
    Use double quote to avoid broken strings in the .py file if description or author name contains a single quote.

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -22,9 +22,9 @@ test_requirements = [
 setup(
     name='{{ cookiecutter.repo_name }}',
     version='{{ cookiecutter.version }}',
-    description='{{ cookiecutter.project_short_description }}',
+    description="{{ cookiecutter.project_short_description }}",
     long_description=readme + '\n\n' + history,
-    author='{{ cookiecutter.full_name }}',
+    author="{{ cookiecutter.full_name }}",
     author_email='{{ cookiecutter.email }}',
     url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
     packages=[
