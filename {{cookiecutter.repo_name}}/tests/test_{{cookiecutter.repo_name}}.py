517d1a0f0f508ead25e0009ab5979799b2fff7dd
Wes Turner <wes.turner@gmail.com>
2015-02-13 10:40:12 -0600

    TST: test_{{cc.repo_name}}.py: setUp then tearDown
    For purposes of test scope, tearDown would be handling attributes configured in setUp,
    which is easier to read when the sequence is setUp, tearDown.

--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
@@ -18,11 +18,12 @@ class Test{{ cookiecutter.repo_name|capitalize }}(unittest.TestCase):
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def test_something(self):
         pass
 
-    def tearDown(self):
-        pass
 
 if __name__ == '__main__':
     unittest.main()
