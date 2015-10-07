38ab000dfcea900a42485f9d2ae019f44cb22e6c
Elias Dorneles <eliasdorneles@gmail.com>
2015-10-06 23:58:39 -0300

    refactor: extracted method for loading key

--- {{cookiecutter.repo_name}}/travis_pypi_setup.py
+++ {{cookiecutter.repo_name}}/travis_pypi_setup.py
@@ -26,18 +26,28 @@ TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
 
+def load_key(pubkey):
+    """Load public RSA key, with work-around for keys using
+    incorrect header/footer format.
+
+    Read more about RSA encryption with cryptography:
+    https://cryptography.io/latest/hazmat/primitives/asymmetric/rsa/
+    """
+    try:
+        return load_pem_public_key(pubkey.encode(), default_backend())
+    except ValueError:
+        # workaround for https://github.com/travis-ci/travis-api/issues/196
+        pubkey = pubkey.replace('BEGIN RSA', 'BEGIN').replace('END RSA', 'END')
+        return load_pem_public_key(pubkey.encode(), default_backend())
+
+
 def encrypt(pubkey, password):
     """Encrypt password using given RSA public key and encode it with base64.
 
     The encrypted password can only be decrypted by someone with the
     private key (in this case, only Travis).
     """
-    try:
-        key = load_pem_public_key(pubkey.encode(), default_backend())
-    except ValueError:
-        # workaround for https://github.com/travis-ci/travis-api/issues/196
-        pubkey = pubkey.replace('BEGIN RSA', 'BEGIN').replace('END RSA', 'END')
-        key = load_pem_public_key(pubkey.encode(), default_backend())
+    key = load_key(pubkey)
     encrypted_password = key.encrypt(password, PKCS1v15())
     return base64.b64encode(encrypted_password)
 
