af6d0a11627cad6b4628927ed730dcd778cbab2e
Elias Dorneles <eliasdorneles@gmail.com>
2015-11-22 16:27:26 -0200

    workaround to use newer pypy (#travis-ci/4576)

---
+++ .travis-workarounds.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+set -x
+
+# workaround for https://github.com/travis-ci/travis-ci/issues/4756
+if [[ "${TOXENV}" == "pypy" ]]; then
+    sudo add-apt-repository -y ppa:pypy/ppa
+    sudo apt-get -qy update
+    sudo apt-get install -y pypy pypy-dev
+    # This is required because we need to get rid of the Travis installed PyPy
+    # or it'll take precedence over the PPA installed one.
+    sudo rm -rf /usr/local/pypy/bin
+fi
