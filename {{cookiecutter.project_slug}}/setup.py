90272b547773428592d8e47e10a421a9e21f71ed
Louis Taylor <louis@kragniz.eu>
2015-12-10 15:28:31 +0000

    Add python 3.5 to trove classifiers

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -51,6 +51,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
     tests_require=test_requirements
