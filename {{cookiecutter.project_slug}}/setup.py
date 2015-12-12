966c4f2847966150b0735ed80ac552a3ebf8843d
Manu Phatak <bionikspoon@gmail.com>
2015-12-12 04:24:24 -0600

    Remove changelog directive.

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
-    history = history_file.read().replace('.. :changelog:', '')
+    history = history_file.read()
 
 requirements = [
     # TODO: put package requirements here
