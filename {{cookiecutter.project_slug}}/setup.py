5bbb223f478456047e3c43f106ba2be5d3de9e28
Alan Kavanagh <alan@unravel.ie>
2016-01-27 15:15:20 +0000

    fix some config issues

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -11,9 +11,6 @@ except ImportError:
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [
     # TODO: put package requirements here
 ]
