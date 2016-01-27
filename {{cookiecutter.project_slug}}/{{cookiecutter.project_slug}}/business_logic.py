73e2f99e3164dfae37291e5c8d6b25a2c35b9715
Alan Kavanagh <alan@unravel.ie>
2016-01-27 10:23:14 +0000

    manipulated default template to fit our need

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/business_logic.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+""" A module docstring is a good idea if it collects more than one class and implements
+a non trivial data treatment
+"""
+
+
+class BusinessProcess(object):
+    """ Data cruncher, it's very hungry
+    """
+
+    def __init__(self, arg1, arg2, kwarg1=None):
+        """ Describe the role of the business process, this will be collected and published in the doc
+        :param string arg1: name of that stuff
+        :param int arg2: the number of things
+        :param list kwarg1: optional list
+        :return:
+        """
+        pass
