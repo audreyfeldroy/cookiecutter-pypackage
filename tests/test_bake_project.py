e73e4e8d12f6845f6d41a5d862be31feb2004f1e
jhonjairoroa87 <jhonjairoroa87@gmail.com>
2016-06-02 15:39:10 -0700

    - added requirements.txt to install project dependencies - added contextmanager to delete temporal files creates by the tests when cookies are baked

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -4,11 +4,14 @@ import os
 import subprocess
 import yaml
 import datetime
-
+from cookiecutter.utils import rmtree
 
 @contextmanager
 def inside_dir(dirpath):
-    "Execute code from inside the given directory"
+    """
+    Execute code from inside the given directory
+    :param dirpath: String, path of the directory the command is being run.
+    """
     old_path = os.getcwd()
     try:
         os.chdir(dirpath)
@@ -16,9 +19,25 @@ def inside_dir(dirpath):
     finally:
         os.chdir(old_path)
 
+@contextmanager
+def bake_in_temp_dir(cookies, *args, **kwargs):
+    """
+    Delete the temporal directory that is created when executing the tests
+    :param cookies: pytest_cookies.Cookies, cookie to be baked and its temporal files will be removed
+    """
+    result = cookies.bake(*args, **kwargs)
+    try:
+        yield result
+    finally:
+        rmtree(str(result.project))
+
 
 def run_inside_dir(command, dirpath):
-    "Run a command from inside a given directory, returning the exit status"
+    """
+    Run a command from inside a given directory, returning the exit status
+    :param command: Command that will be executed
+    :param dirpath: String, path of the directory the command is being run.
+    """
     with inside_dir(dirpath):
         return subprocess.check_call(shlex.split(command))
 
@@ -35,47 +54,50 @@ def test_year_compute_in_license_file(cookies):
 
 
 def test_bake_with_defaults(cookies):
-    result = cookies.bake()
-    assert result.project.isdir()
-    assert result.exit_code == 0
-    assert result.exception is None
 
-    found_toplevel_files = [f.basename for f in result.project.listdir()]
-    assert 'setup.py' in found_toplevel_files
-    assert 'python_boilerplate' in found_toplevel_files
-    assert 'tox.ini' in found_toplevel_files
-    assert 'tests' in found_toplevel_files
-    assert 'travis_pypi_setup.py' in found_toplevel_files
+    with bake_in_temp_dir(cookies) as result:
+        assert result.project.isdir()
+        assert result.exit_code == 0
+        assert result.exception is None
+
+        found_toplevel_files = [f.basename for f in result.project.listdir()]
+        assert 'setup.py' in found_toplevel_files
+        assert 'python_boilerplate' in found_toplevel_files
+        assert 'tox.ini' in found_toplevel_files
+        assert 'tests' in found_toplevel_files
+        assert 'travis_pypi_setup.py' in found_toplevel_files
 
 
 def test_bake_and_run_tests(cookies):
-    result = cookies.bake()
-    assert result.project.isdir()
-    run_inside_dir('python setup.py test', str(result.project)) == 0
+    with bake_in_temp_dir(cookies) as result:
+        assert result.project.isdir()
+        run_inside_dir('python setup.py test', str(result.project)) == 0
+        print("test_bake_and_run_tests path", str(result.project))
 
 def test_bake_withspecialchars_and_run_tests(cookies):
     """Ensure that a `full_name` with double quotes does not break setup.py"""
-    result = cookies.bake(extra_context={'full_name': 'name "quote" name'})
-    assert result.project.isdir()
-    run_inside_dir('python setup.py test', str(result.project)) == 0
+    with bake_in_temp_dir(cookies, extra_context={'full_name': 'name "quote" name'}) as result:
+        assert result.project.isdir()
+        run_inside_dir('python setup.py test', str(result.project)) == 0
 
 def test_bake_and_run_travis_pypi_setup(cookies):
-    # given:
-    result = cookies.bake()
-    project_path = str(result.project)
+    with bake_in_temp_dir(cookies) as result:
+        # given result and :
+        project_path = str(result.project)
 
-    # when:
-    travis_setup_cmd = ('python travis_pypi_setup.py'
-                        ' --repo audreyr/cookiecutter-pypackage --password invalidpass')
-    run_inside_dir(travis_setup_cmd, project_path)
+        # when:
+        travis_setup_cmd = ('python travis_pypi_setup.py'
+                            ' --repo audreyr/cookiecutter-pypackage --password invalidpass')
+        run_inside_dir(travis_setup_cmd, project_path)
 
-    # then:
-    result_travis_config = yaml.load(open(os.path.join(project_path, ".travis.yml")))
-    assert "secure" in result_travis_config["deploy"]["password"],\
-        "missing password config in .travis.yml"
+        # then:
+        result_travis_config = yaml.load(open(os.path.join(project_path, ".travis.yml")))
+        assert "secure" in result_travis_config["deploy"]["password"],\
+            "missing password config in .travis.yml"
+
 
 def test_make_help(cookies):
-    result = cookies.bake()
+    with bake_in_temp_dir(cookies) as result:
+        output = check_output_inside_dir('make help', str(result.project))
+        assert b"check code coverage quickly with the default Python" in output
 
-    output = check_output_inside_dir('make help', str(result.project))
-    assert b"check code coverage quickly with the default Python" in output
