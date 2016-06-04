e13d2080db63a6752aa7a6a33f4912bc2a4a59e1
cerickson <basicsafty@gmail.com>
2016-06-04 19:33:28 -0400

    Added newline to cli for flake8 check

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -6,5 +6,6 @@ def main(args=None):
     click.echo("Add a console script for {{cookiecutter.project_slug}}")
     click.echo("See click documentation at http://http://click.pocoo.org/")
 
+
 if __name__ == "__main__":
     main()
