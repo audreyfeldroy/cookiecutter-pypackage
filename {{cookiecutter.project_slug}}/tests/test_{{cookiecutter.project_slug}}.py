d4076a9257ef153c07f17b854c4d788b5a6e4749
cerickson <basicsafty@gmail.com>
2016-06-06 01:12:19 -0400

    Added optional command line tests in generated application

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -7,16 +7,25 @@ test_{{ cookiecutter.project_slug }}
 
 Tests for `{{ cookiecutter.project_slug }}` module.
 """
-
+from pprint import pprint
 {% if cookiecutter.use_pytest == 'y' -%}
 import pytest
 {% else %}
+import sys
 import unittest
+{%- endif %}
+{%- if cookiecutter.command_line_interface|lower == 'click' %}
+from contextlib import contextmanager
+from click.testing import CliRunner
 {%- endif %}
 
 from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
+{%- if cookiecutter.command_line_interface|lower == 'click' %}
+from {{ cookiecutter.project_slug }} import cli
+{%- endif %}
 
-{% if cookiecutter.use_pytest == 'y' -%}
+
+{%- if cookiecutter.use_pytest == 'y' -%}
 class Test{{ cookiecutter.project_slug|title }}(object):
 
     @classmethod
@@ -25,6 +34,12 @@ class Test{{ cookiecutter.project_slug|title }}(object):
 
     def test_something(self):
         pass
+
+{%- if cookiecutter.command_line_interface|lower == 'click' %}
+    def test_command_line_interface(self):
+        runner = click.CliRunner()
+
+{%- endif %}
 
     @classmethod
     def teardown_class(cls):
@@ -40,9 +55,19 @@ class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
 
     def test_000_something(self):
         pass
+{% if cookiecutter.command_line_interface|lower == 'click' %}
+    def test_command_line_interface(self):
+        runner = CliRunner()
+        result = runner.invoke(cli.main)
+        assert result.exit_code == 0
+        assert 'script for {{ cookiecutter.project_slug }}' in result.output
+        help_result = runner.invoke(cli.main, ['--help'])
+        assert help_result.exit_code == 0
+        assert '--help  Show this message and exit.' in help_result.output
+
+{%- endif %}        
 
 
 if __name__ == '__main__':
-    import sys
     sys.exit(unittest.main())
 {%- endif %}
