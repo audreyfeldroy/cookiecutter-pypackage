3f846388172befbb1f826088d15abb888d949303
Lee Brooks <lee@kagiso.io>
2016-08-04 10:33:14 +0200

    Removed sphinx stuff as in the past we have never used it, and it is doubtful we will use it over a simple readme any time soon

--- {{cookiecutter.project_slug}}/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use {{ cookiecutter.project_name }} in a project::
-
-    import {{ cookiecutter.project_slug }}
--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
 with open('CHANGELOG.rst') as history_file:
