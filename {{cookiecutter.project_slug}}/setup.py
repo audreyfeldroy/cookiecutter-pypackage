599b021f330b6528e0793f1626bdb2bc16c3e1eb
Lee Brooks <lee@kagiso.io>
2016-08-04 10:23:38 +0200

    Rename history to changelog

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('CHANGELOG.rst') as history_file:
     history = history_file.read()
 
 requirements = [
