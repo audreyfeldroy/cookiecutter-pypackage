d04b029351c007c98c392b5176e935813d4c9d12
Adam Chainz <adam@adamj.eu>
2016-08-07 12:24:50 +0100

    Don't include AUTHORS.rst in MANIFEST.in if it's not there

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -132,10 +132,15 @@ def test_bake_without_author_file(cookies):
         doc_files = [f.basename for f in result.project.join('docs').listdir()]
         assert 'authors.rst' not in doc_files
 
-        # Asset there are no spaces in the toc tree
+        # Assert there are no spaces in the toc tree
         docs_index_path = result.project.join('docs/index.rst')
         with open(str(docs_index_path)) as index_file:
             assert 'contributing\n   history' in index_file.read()
+
+        # Check that
+        manifest_path = result.project.join('MANIFEST.in')
+        with open(str(manifest_path)) as manifest_file:
+            assert 'AUTHORS.rst' not in manifest_file.read()
 
 
 def test_make_help(cookies):
