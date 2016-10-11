eca46d7076917bf75435d7d27e3b4350d715f353
Kevin Ndung'u <kevgathuku@gmail.com>
2016-10-11 12:19:23 +0300

    Use triple quotes for author name in __init__.py
    Fixes cases where a SyntaxError is raised when the __author__ variable
    may contain a name with apostrophes.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-__author__ = '{{ cookiecutter.full_name }}'
+__author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
