78b4bc2fd610239a4c4da4e3eafca7eb7e6a55b7
Janusz Skonieczny <git@wooyek.com>
2017-10-05 10:59:13 +0200

    revert to default working o travis

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -27,10 +27,6 @@ def boostrap_venv():
     print("### Bootstrapping virtual environment")
     from subprocess import call
     call(["python3", "-m", "venv", "--clear", ".pve"])
-    call(["ls", "-la"])
-    call(["ls", "-la", ".pve"])
-    call(["ls", "-la", ".pve", "bin"])
-
     call([".pve/bin/pip", "install", "-U", "pip", "setuptools"])
     call([".pve/bin/pip", "install", "-r", "requirements/local.txt"])
 
