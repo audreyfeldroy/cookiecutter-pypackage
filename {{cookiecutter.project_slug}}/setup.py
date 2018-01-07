9451bde630632fbb762200445a71de7ed9150c37
Erwin Janssen <erwinjanssen@outlook.com>
2018-01-07 12:34:48 +0100

    Remove Python 2.6 support
    Python 2.6 is no longer supported by the Python core team. Most tools no
    longer support this version of Python, pip will also drop Python 2.6
    support in a future release. Therefore this version of Python should not
    be included in the generated project.

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -71,7 +71,6 @@ setup(
 {%- endif %}
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
