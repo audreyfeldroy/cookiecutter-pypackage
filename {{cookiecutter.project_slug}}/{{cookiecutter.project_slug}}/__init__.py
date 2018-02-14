c1845dad19db14f53f33220a139b081ae86545fa
David Huard <david.huard@gmail.com>
2018-02-14 10:18:19 -0500

    modified cookiecutter defaults.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -2,6 +2,13 @@
 
 """Top-level package for {{ cookiecutter.project_name }}."""
 
+from .wsgi import application
+from .demo import main
+
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
+
+
+
+
