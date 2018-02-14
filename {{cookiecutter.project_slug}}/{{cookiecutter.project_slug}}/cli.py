955e8cf43dc1914e6e2b475681be2c22782d9903
David Huard <david.huard@gmail.com>
2018-02-14 11:00:49 -0500

    fixed errors in cookie generation

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for {{cookiecutter.project_slug}}."""
+
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for {{cookiecutter.project_slug}}."""
+    click.echo("Replace this message by putting your code into "
+               "{{cookiecutter.project_slug}}.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+    return 0
+
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
