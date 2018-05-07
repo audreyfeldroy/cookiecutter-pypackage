a02a2a4d7118da361ae426dd904c93b119bbe5e0
Pingu Carsti <ehbrecht@dkrz.de>
2018-05-07 19:09:14 +0200

    Fix #15: fix wsgi app init

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/wsgi.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/wsgi.py
@@ -4,11 +4,6 @@ from pywps.app.Service import Service
 from .processes import processes
 
 
-def application(environ, start_response):
-    app = create_app()
-    return app(environ, start_response)
-
-
 def create_app(cfgfiles=None):
     config_files = [os.path.join(os.path.dirname(__file__), 'default.cfg')]
     if cfgfiles:
@@ -17,3 +12,6 @@ def create_app(cfgfiles=None):
         config_files.append(os.environ['PYWPS_CFG'])
     service = Service(processes=processes, cfgfiles=config_files)
     return service
+
+
+application = create_app()
