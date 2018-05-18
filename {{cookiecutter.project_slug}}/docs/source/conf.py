7e26958c733be6163d894ba82fafbfb0f3c8e983
Pingu Carsti <ehbrecht@dkrz.de>
2018-05-18 16:56:36 +0200

    Added initial logo (#25)

--- {{cookiecutter.project_slug}}/docs/source/conf.py
+++ {{cookiecutter.project_slug}}/docs/source/conf.py
@@ -100,6 +100,15 @@ html_theme = 'alabaster'
 #
 # html_theme_options = {}
 
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "_static/birdhouse_logo.svg"
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = "_static/favicon.ico"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
