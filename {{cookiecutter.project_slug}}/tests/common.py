8d48266a67eb31dbbf3378a4eabe743c5fb5370f
Pingu Carsti <ehbrecht@dkrz.de>
2018-06-06 20:03:32 +0200

    Fix #35: fix tests failure on python 3.

--- {{cookiecutter.project_slug}}/tests/common.py
+++ {{cookiecutter.project_slug}}/tests/common.py
@@ -5,7 +5,7 @@ class WpsTestClient(WpsClient):
 
     def get(self, *args, **kwargs):
         query = "?"
-        for key, value in kwargs.iteritems():
+        for key, value in kwargs.items():
             query += "{0}={1}&".format(key, value)
         return super(WpsTestClient, self).get(query)
 
