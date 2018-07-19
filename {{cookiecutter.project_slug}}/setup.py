6ac1bc528c7fbc2a4e282bb1d8674bba0fc3c062
Wes Turner <50891+westurner@users.noreply.github.com>
2018-07-19 16:28:31 -0400

    BLD: setup.py: add Python 3.7 to PyPI classifiers list

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -41,6 +41,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     description="{{ cookiecutter.project_short_description }}",
     {%- if 'no' not in cookiecutter.command_line_interface|lower %}
