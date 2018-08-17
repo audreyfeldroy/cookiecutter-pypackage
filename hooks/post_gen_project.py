3a24e23d1d224efb24802e4ace87ffbb03f0a5ba
Jeremy Carroll <jjc@syapse.com>
2018-08-17 15:59:26 -0700

    Cleanup after testing

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import os
-import io
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -14,13 +13,3 @@ if __name__ == '__main__':
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
         cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
         remove_file(cli_file)
-
-    # Patch up .circleci/config.yaml
-    with io.open(".circleci/config.yaml", "r", encoding="utf-8") as f:
-        circleci = f.read()
-
-    circleci.replace("<{<", "{{")
-    circleci.replace(">}>", "}}")
-
-    with io.open(".circleci/config.yaml", "w", encoding="utf-8") as f:
-        f.write(circleci)
--- {{cookiecutter.project_dash_slug}}/setup.py
+++ {{cookiecutter.project_dash_slug}}/setup.py
@@ -5,25 +5,7 @@
 
 from setuptools import setup, find_packages
 
-requirements = [{%- if cookiecutter.command_line_interface|lower == 'click' %}'Click>=6.0',{%- endif %} ]
-
-setup_requirements = [ ]
-
-test_requirements = [ ]
-
 setup(
-    author="Syapse, Inc.",
-    author_email='noreply@syapse.com',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    description="{{ cookiecutter.project_short_description }}",
     {%- if 'no' not in cookiecutter.command_line_interface|lower %}
     entry_points={
         'console_scripts': [
@@ -31,14 +13,6 @@ setup(
         ],
     },
     {%- endif %}
-    install_requires=requirements,
-    include_package_data=True,
-    keywords='{{ cookiecutter.project_slug }}',
-    name='{{ cookiecutter.project_slug }}',
     packages=find_packages(include=['{{ cookiecutter.project_slug }}']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
-    version='{{ cookiecutter.version }}',
-    zip_safe=False,
+    test_suite='tests'
 )
