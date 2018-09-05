0513ed4f7fc072b3cc772c60b5e2707e4b6a4d90
Pingu Carsti <ehbrecht@dkrz.de>
2018-09-05 17:21:17 +0200

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='cookiecutter-pypackage',
     packages=[],
-    version='0.2.0',
+    version='0.3.0',
     description='Cookiecutter template for a PyWPS service',
     license='BSD',
     author='Birdhouse',
