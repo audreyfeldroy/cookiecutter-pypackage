8650f21de75207df3ea35799a0de34bf86885ad0
Chris Lunsford <chrlunsf@cisco.com>
2018-12-18 15:18:46 -0500

    Add Cisco Sample Code License Headers

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
+"""Top-level package for {{ cookiecutter.project_name }}.
 
-"""Top-level package for {{ cookiecutter.project_name }}."""
+Copyright (c) {% now 'local', '%Y' %} Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.0 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
 
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
+__copyright__ = "Copyright (c) {% now 'local', '%Y' %} Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.0"
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
+"""Console script for {{cookiecutter.project_slug}}.
 
-"""Console script for {{cookiecutter.project_slug}}."""
+Copyright (c) {% now 'local', '%Y' %} Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.0 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+
 import sys
 import click
 
