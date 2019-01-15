ed5d2f95ad72077f9ac22fc35fcd500f4510a7c7
David Mougeolle <david.mougeolle@moodule.net>
2019-01-15 23:57:07 +0100

    distinguished the repository from the module

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,10 +4,10 @@ import sys
 
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
-module_name = '{{ cookiecutter.github_repository}}'
+module_name = '{{ cookiecutter.module_name }}'
 
-#if not re.match(MODULE_REGEX, module_name):
-#    print('ERROR: The project slug (%s) is not a valid Python module name. Please do not use a - and use _ instead' % module_name)
-#
-#    #Exit to cancel project
-#    sys.exit(1)
+if not re.match(MODULE_REGEX, module_name):
+    print('ERROR: The project slug (%s) is not a valid Python module name. Please do not use a - and use _ instead' % module_name)
+
+    # Exit to cancel project
+    sys.exit(1)
