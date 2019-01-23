da894f146c1c066f5ce464a40c659b5496982dff
Donat Perler <donat.perler@gmail.com>
2019-01-23 15:50:48 +0100

    Update cli.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -3,7 +3,8 @@
 import sys
 import click
 import logging
-from utils import countToLogLevel
+
+__version__ = '{{ cookiecutter.version }}'
 
 
 @click.command()
@@ -14,21 +15,42 @@ from utils import countToLogLevel
     default=False,
     help="Perform a trial run with no changes made")
 @click.option('--verbose', '-v', count=True, help="Increase verbosity")
-@click.option('--version', '-V', help="Print version")
-def main(is_dry_run, verbose):
-    """Console script for {{cookiecutter.project_slug}}."""
-    logger = logging.getLogger()
-    logger.setLevel(countToLogLevel(verbose))
-    logger.error("This is an error message")
-    logger.warning("This is a warning")
-    logger.info("This is an info message")
-    logger.info("This is a debug message")
+@click.option('--version', '-V', is_flag=True, help="Print version")
+def main(*args, **kwargs):
+    """Console script for test_cli_project."""
+
+    logging.basicConfig(level=countToLogLevel(kwargs['verbose']))
+
+    logging.warning("This is a warning.")
+    logging.info("This is an info message.")
+    logging.debug("This is a debug message.")
+
+    if kwargs['version']:
+        click.echo(__version__)
+        return 0
+
+    if kwargs['dry_run']:
+        click.echo("Is dry run")
+        return 0
+
     click.echo(
-        "Replace this message by putting your code into {{cookiecutter.project_slug}}.cli.main"
+        "Replace this message by putting your code into test_cli_project.cli.main"
     )
     click.echo("See click documentation at http://click.pocoo.org/")
 
     return 0
+
+
+def countToLogLevel(count: int) -> int:
+    """Map the occurence of the command line option verbose to the log level"""
+    if count == 0:
+        return logging.ERROR
+    elif count == 1:
+        return logging.WARNING
+    elif count == 2:
+        return logging.INFO
+    else:
+        return logging.DEBUG
 
 
 if __name__ == "__main__":
