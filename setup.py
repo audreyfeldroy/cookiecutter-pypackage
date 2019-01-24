d27317c06a5c66a2ea2968d65394c0739738e47b
briggySmalls <wasso14@hotmail.com>
2019-01-24 00:06:51 +0000

    Perform initial rework of project
    Note: no attempts to fix tests yet

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     packages=[],
     version='0.1.0',
     description='Cookiecutter template for a Python package',
-    author='Audrey Roy Greenfeld',
+    author='Sam Briggs',
     license='BSD',
-    author_email='aroy@alum.mit.edu',
-    url='https://github.com/audreyr/cookiecutter-pypackage',
+    author_email='briggySmalls90@gmail.com',
+    url='https://github.com/briggySmalls/cookiecutter-pypackage',
     keywords=['cookiecutter', 'template', 'package', ],
     classifiers=[
         'Development Status :: 4 - Beta',
