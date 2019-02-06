090ef4c6d43fff416ad53e9be1d3adf17e1c0f7c
Donat Perler <donat.perler@gmail.com>
2019-02-06 14:16:07 +0100

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -13,6 +13,9 @@ if __name__ == '__main__':
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
         cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
         utils_file = os.path.join('{{ cookiecutter.project_slug }}', 'utils.py')
+        test_cli_file = os.path.join('tests', 'test_cli.py')
+        test_utils_file = os.path.join('tests', 'test_utils.py')
         remove_file(cli_file)
         remove_file(utils_file)
-
+        remove_file(test_cli_file)
+        remove_file(test_utils_file)
