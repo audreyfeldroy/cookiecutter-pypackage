53747b5a6eaf2dabf60add361b949db6bbf3a6aa
Donat Perler <donat.perler@gmail.com>
2019-02-06 14:11:41 +0100

    Update test_{{cookiecutter.project_slug}}.py

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -2,15 +2,8 @@
 # -*- coding: utf-8 -*-
 """Tests for `{{ cookiecutter.project_slug }}` package."""
 import pytest
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-import logging
-from click.testing import CliRunner
-{%- endif %}
 
 from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-from {{ cookiecutter.project_slug }} import cli
-from {{ cookiecutter.project_slug }} import utils{%- endif %}
 
 
 @pytest.fixture
@@ -27,29 +20,3 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'test_cli_project.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert 'Show this message and exit.' in help_result.output
-    dry_run_result = runner.invoke(cli.main, ['-n'])
-    assert dry_run_result.exit_code == 0
-    assert 'Is dry run' in dry_run_result.output
-    version_result = runner.invoke(cli.main, ['-V'])
-    assert version_result.exit_code == 0
-    assert cli.__version__ in version_result.output
-
-
-def test_count_to_log_level():
-    assert utils.count_to_log_level(0) == logging.ERROR
-    assert utils.count_to_log_level(1) == logging.WARNING
-    assert utils.count_to_log_level(2) == logging.INFO
-    assert utils.count_to_log_level(3) == logging.DEBUG
-{%- endif %}
