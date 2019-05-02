8675e0f5244c937535059126ea86bc985b7f268e
Zane Dufour <zane.dufour@gmail.com>
2019-05-01 22:55:22 -0400

    Attempt to add __main__.py to template

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -19,6 +19,7 @@ if __name__ == '__main__':
 
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
         cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
+        main_file = os.path.join('{{ cookiecutter.project_slug }}', '__main__.py')
         remove_file(cli_file)
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
@@ -0,0 +1,7 @@
+"""Script that is called by python -m {{cookiecutter.project_slug}}"""
+import sys
+from .cli import main
+
+if __name__ == '__main__':
+    sys.exit(main())
+
