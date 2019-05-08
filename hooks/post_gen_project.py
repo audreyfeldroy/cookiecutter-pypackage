6ea2aaec21f31f4e29044fcdc34f7109c8fce3e0
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2019-05-08 21:23:16 +0200

    Use symlinks to include markdown, move requirements_dev.txt to setup.cfg
    Remove python 2 support, fix hook for no author file
    
    Python 3.5 symlink str
    
    Use dir_fd argument instead of chdir
    
    Proper fix for python<3.6

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+from pathlib import Path
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -8,11 +9,18 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
+def make_symlink(filename, directory):
+    os.symlink(
+        str(Path('..') / filename),  # python < 3.6
+        filename.lower(),
+        dir_fd=directory
+    )
+
+
 if __name__ == '__main__':
 
     if '{{ cookiecutter.create_author_file }}' != 'y':
         remove_file('AUTHORS.md')
-        remove_file('docs/authors.rst')
 
     if '{{ cookiecutter.use_pytest }}' == 'y':
         remove_file('tests/__init__.py')
@@ -23,3 +31,14 @@ if __name__ == '__main__':
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
+
+    markdown_files = Path.cwd().glob("*.md")
+    docs = os.open('docs', os.O_RDONLY)
+    for file in markdown_files:
+        symlink = Path('docs') / file.name.lower()
+        if symlink.exists():
+            symlink.unlink()  # remove
+
+        make_symlink(file.name, docs)
+
+    os.close(docs)
