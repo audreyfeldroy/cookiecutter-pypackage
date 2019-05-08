feee50284f773a46d21949f8cac29e1f01bee2dd
Shaun Martin <shaun@samsite.ca>
2019-05-08 17:54:10 -0500

    missed a couple

---
+++ {{cookiecutter.project_slug}}/tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Test package for {{ cookiecutter.project_name }}."""
+
+__author__ = """{{ cookiecutter.full_name }}"""
+__email__ = '{{ cookiecutter.email }}'
+__version__ = '{{ cookiecutter.version }}'
