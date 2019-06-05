e0ba3fe5155c00a392e19dab14680a32a01e2be1
Sander van Dorsten <Sander-van.Dorsten@klm.com>
2019-06-05 22:59:48 +0200

    edited standard settings cookiecutter

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -18,8 +18,9 @@ if __name__ == '__main__':
         remove_file('tests/__init__.py')
 
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
-        remove_file(cli_file)
+        # cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
+        # remove_file(cli_file)
+        pass
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
