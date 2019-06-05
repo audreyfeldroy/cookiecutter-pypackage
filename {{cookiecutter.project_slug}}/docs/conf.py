3052734c375610c0a63f1c7d2e5349607e877ad4
Sander van Dorsten <Sander-van.Dorsten@klm.com>
2019-06-05 22:31:34 +0200

    docs: added napoleon package for GOOgle style docstring formatting

--- {{cookiecutter.project_slug}}/docs/conf.py
+++ {{cookiecutter.project_slug}}/docs/conf.py
@@ -40,6 +40,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.coverage',
               'sphinx.ext.mathjax',
               'sphinx.ext.viewcode',
+              'sphinxcontrib.napoleon'
               ]
 
 # Add any paths that contain templates here, relative to this directory.
