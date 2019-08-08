8395c67283ef43fb4713935e79f88a8efc4409ce
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2019-08-08 14:42:57 +0200

    Replace setup.py with setup.cfg

--- setup.py
+++ setup.py
@@ -1,29 +1,7 @@
 # !/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
+
 setup(
-    name='cookiecutter-pypackage',
-    packages=[],
-    version='0.2.1',
-    description='Cookiecutter template for a Python package',
-    author='Audrey Roy Greenfeld',
-    license='BSD',
-    author_email='aroy@alum.mit.edu',
-    url='https://github.com/audreyr/cookiecutter-pypackage',
-    keywords=['cookiecutter', 'template', 'package', ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development',
-    ],
+    use_scm_version=True
 )
