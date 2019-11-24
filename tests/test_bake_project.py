5b1c7df7f8c70b2aabae5719e4f5a4f2ad8c2d6e
Chad Bailey <chadbailey904@gmail.com>
2019-11-23 19:42:23 -0600

    Fixed flake8 linting errors, fixed test_make_help failing on windows due to platform incompatibility, added .vscode to gitignore

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -96,14 +96,20 @@ def test_bake_and_run_tests(cookies):
 
 def test_bake_withspecialchars_and_run_tests(cookies):
     """Ensure that a `full_name` with double quotes does not break setup.py"""
-    with bake_in_temp_dir(cookies, extra_context={'full_name': 'name "quote" name'}) as result:
+    with bake_in_temp_dir(
+        cookies,
+        extra_context={'full_name': 'name "quote" name'}
+    ) as result:
         assert result.project.isdir()
         run_inside_dir('python setup.py test', str(result.project)) == 0
 
 
 def test_bake_with_apostrophe_and_run_tests(cookies):
     """Ensure that a `full_name` with apostrophes does not break setup.py"""
-    with bake_in_temp_dir(cookies, extra_context={'full_name': "O'connor"}) as result:
+    with bake_in_temp_dir(
+        cookies,
+        extra_context={'full_name': "O'connor"}
+    ) as result:
         assert result.project.isdir()
         run_inside_dir('python setup.py test', str(result.project)) == 0
 
@@ -115,24 +121,38 @@ def test_bake_with_apostrophe_and_run_tests(cookies):
 #
 #         # when:
 #         travis_setup_cmd = ('python travis_pypi_setup.py'
-#                             ' --repo audreyr/cookiecutter-pypackage --password invalidpass')
+#                             ' --repo audreyr/cookiecutter-pypackage'
+#                             ' --password invalidpass')
 #         run_inside_dir(travis_setup_cmd, project_path)
 #         # then:
-#         result_travis_config = yaml.load(result.project.join(".travis.yml").open())
+#         result_travis_config = yaml.load(
+#             result.project.join(".travis.yml").open()
+#         )
 #         min_size_of_encrypted_password = 50
-#         assert len(result_travis_config["deploy"]["password"]["secure"]) > min_size_of_encrypted_password
+#         assert len(
+#             result_travis_config["deploy"]["password"]["secure"]
+#         ) > min_size_of_encrypted_password
 
 
 def test_bake_without_travis_pypi_setup(cookies):
-    with bake_in_temp_dir(cookies, extra_context={'use_pypi_deployment_with_travis': 'n'}) as result:
-        result_travis_config = yaml.load(result.project.join(".travis.yml").open(), Loader=yaml.FullLoader)
+    with bake_in_temp_dir(
+        cookies,
+        extra_context={'use_pypi_deployment_with_travis': 'n'}
+    ) as result:
+        result_travis_config = yaml.load(
+            result.project.join(".travis.yml").open(),
+            Loader=yaml.FullLoader
+        )
         assert "deploy" not in result_travis_config
         assert "python" == result_travis_config["language"]
-        found_toplevel_files = [f.basename for f in result.project.listdir()]
+        # found_toplevel_files = [f.basename for f in result.project.listdir()]
 
 
 def test_bake_without_author_file(cookies):
-    with bake_in_temp_dir(cookies, extra_context={'create_author_file': 'n'}) as result:
+    with bake_in_temp_dir(
+        cookies,
+        extra_context={'create_author_file': 'n'}
+    ) as result:
         found_toplevel_files = [f.basename for f in result.project.listdir()]
         assert 'AUTHORS.rst' not in found_toplevel_files
         doc_files = [f.basename for f in result.project.join('docs').listdir()]
@@ -151,26 +171,40 @@ def test_bake_without_author_file(cookies):
 
 def test_make_help(cookies):
     with bake_in_temp_dir(cookies) as result:
-        output = check_output_inside_dir('make help', str(result.project))
-        assert b"check code coverage quickly with the default Python" in output
+        # The supplied Makefile does not support win32
+        if sys.platform != "win32":
+            output = check_output_inside_dir(
+                'make help',
+                str(result.project)
+            )
+            assert b"check code coverage quickly with the default Python" in \
+                output
 
 
 def test_bake_selecting_license(cookies):
     license_strings = {
         'MIT license': 'MIT ',
-        'BSD license': 'Redistributions of source code must retain the above copyright notice, this',
+        'BSD license': 'Redistributions of source code must retain the ' +
+                       'above copyright notice, this',
         'ISC license': 'ISC License',
-        'Apache Software License 2.0': 'Licensed under the Apache License, Version 2.0',
+        'Apache Software License 2.0':
+            'Licensed under the Apache License, Version 2.0',
         'GNU General Public License v3': 'GNU GENERAL PUBLIC LICENSE',
     }
     for license, target_string in license_strings.items():
-        with bake_in_temp_dir(cookies, extra_context={'open_source_license': license}) as result:
+        with bake_in_temp_dir(
+            cookies,
+            extra_context={'open_source_license': license}
+        ) as result:
             assert target_string in result.project.join('LICENSE').read()
             assert license in result.project.join('setup.py').read()
 
 
 def test_bake_not_open_source(cookies):
-    with bake_in_temp_dir(cookies, extra_context={'open_source_license': 'Not open source'}) as result:
+    with bake_in_temp_dir(
+        cookies,
+        extra_context={'open_source_license': 'Not open source'}
+    ) as result:
         found_toplevel_files = [f.basename for f in result.project.listdir()]
         assert 'setup.py' in found_toplevel_files
         assert 'LICENSE' not in found_toplevel_files
@@ -178,9 +212,14 @@ def test_bake_not_open_source(cookies):
 
 
 def test_using_pytest(cookies):
-    with bake_in_temp_dir(cookies, extra_context={'use_pytest': 'y'}) as result:
+    with bake_in_temp_dir(
+        cookies,
+        extra_context={'use_pytest': 'y'}
+    ) as result:
         assert result.project.isdir()
-        test_file_path = result.project.join('tests/test_python_boilerplate.py')
+        test_file_path = result.project.join(
+            'tests/test_python_boilerplate.py'
+        )
         lines = test_file_path.readlines()
         assert "import pytest" in ''.join(lines)
         # Test the new pytest target
@@ -192,24 +231,31 @@ def test_using_pytest(cookies):
 def test_not_using_pytest(cookies):
     with bake_in_temp_dir(cookies) as result:
         assert result.project.isdir()
-        test_file_path = result.project.join('tests/test_python_boilerplate.py')
+        test_file_path = result.project.join(
+            'tests/test_python_boilerplate.py'
+        )
         lines = test_file_path.readlines()
         assert "import unittest" in ''.join(lines)
         assert "import pytest" not in ''.join(lines)
 
 
 # def test_project_with_hyphen_in_module_name(cookies):
-#     result = cookies.bake(extra_context={'project_name': 'something-with-a-dash'})
+#     result = cookies.bake(
+#         extra_context={'project_name': 'something-with-a-dash'}
+#     )
 #     assert result.project is not None
 #     project_path = str(result.project)
 #
 #     # when:
 #     travis_setup_cmd = ('python travis_pypi_setup.py'
-#                         ' --repo audreyr/cookiecutter-pypackage --password invalidpass')
+#                         ' --repo audreyr/cookiecutter-pypackage'
+#                         ' --password invalidpass')
 #     run_inside_dir(travis_setup_cmd, project_path)
 #
 #     # then:
-#     result_travis_config = yaml.load(open(os.path.join(project_path, ".travis.yml")))
+#     result_travis_config = yaml.load(
+#         open(os.path.join(project_path, ".travis.yml"))
+#     )
 #     assert "secure" in result_travis_config["deploy"]["password"],\
 #         "missing password config in .travis.yml"
 
@@ -268,7 +314,9 @@ def test_bake_with_console_script_cli(cookies):
     runner = CliRunner()
     noarg_result = runner.invoke(cli.main)
     assert noarg_result.exit_code == 0
-    noarg_output = ' '.join(['Replace this message by putting your code into', project_slug])
+    noarg_output = ' '.join([
+        'Replace this message by putting your code into',
+        project_slug])
     assert noarg_output in noarg_result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
@@ -293,7 +341,9 @@ def test_bake_with_argparse_console_script_cli(cookies):
     runner = CliRunner()
     noarg_result = runner.invoke(cli.main)
     assert noarg_result.exit_code == 0
-    noarg_output = ' '.join(['Replace this message by putting your code into', project_slug])
+    noarg_output = ' '.join([
+        'Replace this message by putting your code into',
+        project_slug])
     assert noarg_output in noarg_result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
