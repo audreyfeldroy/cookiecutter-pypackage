4bf16de9083dc61463c8fe5fd1d97cb5dbd52af2
Joseph <joseph.egan@ukho.gov.uk>
2020-03-11 11:08:22 +0000

    Add coverage check to pytest (#6)
    * Add coverage check to pytest
    * Add example test and fix coverage target in setup.cfg
    * Refactor requirements-dev.txt to point to the templates deps
    * Add test command to makefile

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -36,7 +36,6 @@ setup(
 {%- endif %}
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
