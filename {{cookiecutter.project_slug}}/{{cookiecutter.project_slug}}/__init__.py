05e5ed7bba3a271b512e7bd03a0fc2f5b7c3cf33
moodule <moodule@protonmail.com>
2020-09-13 15:48:45 +0200

    Remove 'bump2version' dep, 'poetry version xx' takes care of everything

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for {{ cookiecutter.project_name }}."""
 
+from importlib.metadata import version
+
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
-__version__ = '{{ cookiecutter.version }}'
+__version__ = version(__package__.split('.')[0])
