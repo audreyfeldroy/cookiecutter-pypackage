358f85366ee82d6d01184099ec28033351593dac
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2020-11-02 12:10:56 +0100

    moving back to github

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -26,7 +26,7 @@ def response():
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
     # import requests
-    # return requests.get('https://git.rwth-aachen.de/mbe/cookiecutter-pypackage')
+    # return requests.get('https://github.com/mberz/cookiecutter-pypackage')
 
 
 def test_content(response):
