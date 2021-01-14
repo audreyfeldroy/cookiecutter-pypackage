51f7c41649456ec97931217b1c87a4e666df3e6b
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2021-01-14 16:02:30 -0500

    Added various fixes for including/removing docs in the project initiation phase

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,11 +8,27 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
+def remove_folder(folderpath):
+    for file in os.listdir(folderpath):
+        if os.path.isfile(file):
+            os.remove(os.path.join(PROJECT_DIRECTORY, folderpath, file))
+            continue
+        try:
+            os.rmdir(os.path.join(PROJECT_DIRECTORY, folderpath, file))
+        except OSError:
+            pass
+    os.rmdir(os.path.join(PROJECT_DIRECTORY, folderpath))
+
+
 if __name__ == '__main__':
 
     if '{{ cookiecutter.create_author_file }}' != 'y':
         remove_file('AUTHORS.rst')
         remove_file('docs/authors.rst')
+
+    if '{{ cookiecutter.make_docs }}' != 'y':
+        remove_file('requirements_docs.txt')
+        remove_folder('docs')
 
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
         cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
