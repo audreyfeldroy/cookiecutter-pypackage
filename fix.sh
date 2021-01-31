3a983ebe813b9a67592e80b82ca3fe344cbb0525
Vince Broz <vince@broz.cc>
2021-01-30 23:52:52 -0500

    Import fixes back into primary fix.sh

--- fix.sh
+++ fix.sh
@@ -66,7 +66,7 @@ ensure_bundle() {
   bundle install
 }
 
-latest_version() {
+latest_python_version() {
   major_minor=${1}
   # https://stackoverflow.com/questions/369758/how-to-trim-whitespace-from-a-bash-variable
   pyenv install --list | grep "^  ${major_minor}." | grep -v -- -dev | tail -1 | xargs
@@ -79,11 +79,13 @@ install_pyenv() {
     if ! type pyenv 2>/dev/null
     then
       # https://github.com/pyenv/pyenv-installer/blob/master/bin/pyenv-installer
-      2>&1 cat <<EOF
+      >&2 cat <<EOF
 WARNING: seems you still have not added 'pyenv' to the load path.
+
 # Load pyenv automatically by adding
 # the following to ~/.bashrc:
-export PATH="/home/circleci/.pyenv/bin:$PATH"
+
+export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 EOF
@@ -116,44 +118,64 @@ ensure_pyenv() {
   fi
 }
 
-ensure_pyenv
-
 # You can find out which feature versions are still supported / have
 # been release here: https://www.python.org/downloads/
-python_versions="$(latest_version 3.9) $(latest_version 3.8) $(latest_version 3.7) $(latest_version 3.6)"
+ensure_python_versions() {
+  # You can find out which feature versions are still supported / have
+  # been release here: https://www.python.org/downloads/
+  python_versions="$(latest_python_version 3.9) $(latest_python_version 3.8) $(latest_python_version 3.7) $(latest_python_version 3.6)"
 
-echo "Latest Python versions: ${python_versions}"
+  echo "Latest Python versions: ${python_versions}"
 
-for ver in $python_versions
-do
-  if [ "$(uname)" == Darwin ]
-  then
-    if ! [ -f /usr/local/opt/zlib/lib/libz.dylib ]
+  for ver in $python_versions
+  do
+    if [ "$(uname)" == Darwin ]
     then
-      # https://teratail.com/questions/309663
-      HOMEBREW_NO_AUTO_UPDATE=1 brew install zlib || true
-    fi
-    if ! [ -f /usr/local/opt/bzip2/bin/bzip2 ]
-    then
-      # https://teratail.com/questions/309663
-      HOMEBREW_NO_AUTO_UPDATE=1 brew install bzip2 || true
-    fi
+      if ! [ -f /usr/local/opt/zlib/lib/libz.dylib ]
+      then
+        # https://teratail.com/questions/309663
+        HOMEBREW_NO_AUTO_UPDATE=1 brew install zlib || true
+      fi
+      if ! [ -f /usr/local/opt/bzip2/bin/bzip2 ]
+      then
+        # https://teratail.com/questions/309663
+        HOMEBREW_NO_AUTO_UPDATE=1 brew install bzip2 || true
+      fi
 
-    pyenv_install() {
-      CFLAGS="-I/usr/local/opt/zlib/include -I/usr/local/opt/bzip2/include" LDFLAGS="-L/usr/local/opt/zlib/lib -L/usr/local/opt/bzip2/lib" pyenv install --skip-existing "$@"
-    }
+      pyenv_install() {
+        CFLAGS="-I/usr/local/opt/zlib/include -I/usr/local/opt/bzip2/include" LDFLAGS="-L/usr/local/opt/zlib/lib -L/usr/local/opt/bzip2/lib" pyenv install --skip-existing "$@"
+      }
 
-    major_minor="$(cut -d. -f1-2 <<<"${ver}")"
-    if [ "${major_minor}" == 3.6 ]
-    then
-      pyenv_install --patch "${ver}" < <(curl -sSL https://github.com/python/cpython/commit/8ea6353.patch\?full_index=1)
+      major_minor="$(cut -d. -f1-2 <<<"${ver}")"
+      if [ "${major_minor}" == 3.6 ]
+      then
+        pyenv_install --patch "${ver}" < <(curl -sSL https://github.com/python/cpython/commit/8ea6353.patch\?full_index=1)
+      else
+        pyenv_install "${ver}"
+      fi
     else
-      pyenv_install "${ver}"
+      pyenv install -s "${ver}"
     fi
-  else
-    pyenv install -s "${ver}"
+  done
+}
+
+install_shellcheck() {
+  if [ "$(uname)" == "Darwin" ]
+  then
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install check || true
+  elif type apt-get >/dev/null 2>&1
+  then
+    sudo apt-get update -y
+    sudo apt-get install shellcheck
   fi
-done
+}
+
+ensure_shellcheck() {
+  if ! type shellcheck >/dev/null 2>&1
+  then
+    install_shellcheck
+  fi
+}
 
 ensure_rbenv
 
@@ -161,10 +183,19 @@ ensure_ruby_version
 
 ensure_bundle
 
+ensure_pyenv
+
+ensure_python_versions
+
+ensure_shellcheck
+
+
+
 
 latest_python_version="$(cut -d' ' -f1 <<< "${python_versions}")"
 virtualenv_name="cookiecutter-pypackage-${latest_python_version}"
 pyenv virtualenv "${latest_python_version}" "${virtualenv_name}" || true
+# You can use this for your global stuff!
 pyenv virtualenv mylibs || true
 pyenv local "${virtualenv_name}" ${python_versions} mylibs
 # Make sure we have a pip with the 20.3 resolver, and after the
