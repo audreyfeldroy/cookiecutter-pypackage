2e4ef59a7c8de0ac63f6b8b03aeebe3bcf2890fb
Vince Broz <vince@broz.cc>
2021-01-30 23:30:22 -0500

    Add installation of pyenv to fix.sh in created projects (#38)

--- {{cookiecutter.project_slug}}/fix.sh
+++ {{cookiecutter.project_slug}}/fix.sh
@@ -72,6 +72,52 @@ latest_version() {
   pyenv install --list | grep "^  ${major_minor}." | grep -v -- -dev | tail -1 | xargs
 }
 
+install_pyenv() {
+  if [ "$(uname)" == "Darwin" ]
+  then
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install pyenv || true
+    if ! type pyenv 2>/dev/null
+    then
+      # https://github.com/pyenv/pyenv-installer/blob/master/bin/pyenv-installer
+      2>&1 cat <<EOF
+WARNING: seems you still have not added 'pyenv' to the load path.
+# Load pyenv automatically by adding
+# the following to ~/.bashrc:
+export PATH="/home/circleci/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+EOF
+    fi
+  else
+    curl https://pyenv.run | bash
+  fi
+}
+
+set_pyenv_env_variables() {
+  # looks like pyenv scripts aren't -u clean:
+  #
+  # https://app.circleci.com/pipelines/github/apiology/cookiecutter-pypackage/15/workflows/10506069-7662-46bd-b915-2992db3f795b/jobs/15
+  set +u
+  export PATH="${HOME}/.pyenv/bin:$PATH"
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
+  set -u
+}
+
+ensure_pyenv() {
+  if ! type pyenv >/dev/null 2>&1 && ! [ -f "${HOME}/.pyenv/bin/pyenv" ]
+  then
+    install_pyenv
+  fi
+
+  if ! type pyenv >/dev/null 2>&1
+  then
+    set_pyenv_env_variables
+  fi
+}
+
+ensure_pyenv
+
 # You can find out which feature versions are still supported / have
 # been release here: https://www.python.org/downloads/
 python_versions="$(latest_version 3.9) $(latest_version 3.8) $(latest_version 3.7) $(latest_version 3.6)"
