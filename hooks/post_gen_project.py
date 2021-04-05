fcc78b1f53385d1dcbc4784a7e43fb697e4e4306
Vince Broz <vince@broz.cc>
2021-04-05 14:02:05 -0400

    Don't install CircleCI if I don't have fix.sh

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -35,6 +35,9 @@ if __name__ == '__main__':
         # multiple times over its lifetime.
         subprocess.check_call(['git', 'init'])
         subprocess.check_call(['git', 'add', '-A'])
+
+    if (os.environ.get('SKIP_FIX_SCRIPT', '0') != '1' and
+       os.environ.get('SKIP_GIT_CREATION', '0') != '1'):
         subprocess.check_call(['bundle', 'exec', 'overcommit', '--install'])
         subprocess.check_call(['bundle', 'exec', 'overcommit', '--sign'])
         subprocess.check_call(['bundle', 'exec', 'overcommit', '--sign', 'pre-commit'])
