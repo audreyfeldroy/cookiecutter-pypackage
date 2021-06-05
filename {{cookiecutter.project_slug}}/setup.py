491a716ff37a00e21f4274db66f0329fbe0f3817
Vince Broz <vince@broz.cc>
2021-06-05 19:04:34 -0400

    Add mypy and unit test coverage ratcheting (#105)
    * Add mypy and test coverage ratcheting to child projects
    
    * Fix blank line count for flake8

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -3,9 +3,23 @@
 
 """The setup script."""
 
+from decimal import Decimal
+import os
+import os.path
 from typing import List
 
+# This must be about distutils, despite flake8's opinions.  Otherwise,
+# this diagnostic is emitted:
+
+# UserWarning: Distutils was imported before Setuptools. This usage is
+# discouraged and may exhibit undesirable behaviors or errors. Please
+# use Setuptools' objects directly or at least import Setuptools
+# first.
 from setuptools import find_packages, setup
+
+
+from distutils.cmd import Command  # noqa: I100
+
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -24,6 +38,70 @@ test_requirements: List[str] = ['pytest>=3']
     'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
     'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
 } %}
+
+
+# From https://github.com/bluelabsio/records-mover/blob/master/setup.py
+class CoverageRatchetCommand(Command):
+    description = 'Run coverage ratchet'
+    user_options = []  # type: ignore
+    coverage_file: str
+    coverage_source_file: str
+    coverage_url: str
+    type_of_coverage: str
+
+    def finalize_options(self) -> None:
+        pass
+
+    def run(self) -> None:
+        """Run command."""
+        import xml.etree.ElementTree as ET
+
+        tree = ET.parse(self.coverage_source_file)
+        new_coverage = Decimal(tree.getroot().attrib["line-rate"]) * 100
+
+        if not os.path.exists(self.coverage_file):
+            with open(self.coverage_file, 'w') as f:
+                f.write('0')
+
+        with open(self.coverage_file, 'r') as f:
+            high_water_mark = Decimal(f.read())
+
+        if new_coverage < high_water_mark:
+            raise Exception(
+                f"{self.type_of_coverage} coverage used to be {high_water_mark}; "
+                f"down to {new_coverage}%.  Fix by viewing '{self.coverage_url}'")
+        elif new_coverage > high_water_mark:
+            with open(self.coverage_file, 'w') as f:
+                f.write(str(new_coverage))
+            print(f"Just ratcheted coverage up to {new_coverage}%")
+        else:
+            print(f"Code coverage steady at {new_coverage}%")
+
+
+class TestCoverageRatchetCommand(CoverageRatchetCommand):
+    def initialize_options(self) -> None:
+        """Set default values for options."""
+        self.type_of_coverage = 'Test'
+        self.coverage_url = 'cover/index.html'
+        self.coverage_file = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'metrics',
+            'coverage_high_water_mark'
+        )
+        self.coverage_source_file = "coverage.xml"
+
+
+class MypyCoverageRatchetCommand(CoverageRatchetCommand):
+    def initialize_options(self) -> None:
+        """Set default values for options."""
+        self.type_of_coverage = 'Mypy'
+        self.coverage_url = 'typecover/index.html'
+        self.coverage_file = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'metrics',
+            'mypy_high_water_mark'
+        )
+        self.coverage_source_file = "typecover/cobertura.xml"
 
 
 setup(
@@ -51,6 +129,10 @@ setup(
         ],
     },
     {%- endif %}
+    cmdclass={
+        'coverage_ratchet': TestCoverageRatchetCommand,
+        'mypy_ratchet': MypyCoverageRatchetCommand,
+    },
     install_requires=requirements,
 {%- if cookiecutter.open_source_license in license_classifiers %}
     license="{{ cookiecutter.open_source_license }}",
