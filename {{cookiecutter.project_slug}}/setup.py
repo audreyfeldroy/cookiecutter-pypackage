19a9a867f7beac2ee0ca39f9f916e413411e698b
Dougal Scott <dougal.scott@intelematics.com>
2021-08-23 12:13:43 +1000

    Minor tweaks

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [{%- if cookiecutter.command_line_interface|lower == 'click' %}'Click>=7.0',{%- endif %} ]
+with open("requirements.txt") as requirements_file:
+    requirements = requirements_file.readlines()
 
 test_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest>=3',{%- endif %} ]
 
