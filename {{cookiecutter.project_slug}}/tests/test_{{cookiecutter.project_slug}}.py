0fba0af678a24aaed96ac4243e98ca089ed2d5a2
Biel Stela <biel.stela@gmail.com>
2022-05-26 12:10:50 +0200

    Fixes deps, template python style and adds make style to format code and sort imports

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -6,17 +6,11 @@ import pytest
 
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 from click.testing import CliRunner
-
 {%- endif %}
-
 from {{cookiecutter.project_slug}} import {{ cookiecutter.project_slug }}
-
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 from {{cookiecutter.project_slug}} import cli
-
 {%- endif %}
-
-
 
 
 @pytest.fixture
@@ -33,15 +27,16 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
+
+
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert '{{ cookiecutter.project_slug }}.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
+    assert "{{ cookiecutter.project_slug }}.cli.main" in result.output
+    help_result = runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert "--help  Show this message and exit." in help_result.output
 {%- endif %}
