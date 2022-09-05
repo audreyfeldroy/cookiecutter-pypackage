72553f6e3034a722f22f076c70811c96e7bb3f9e
mazzma12 <matthieu@crowdsec.net>
2022-09-05 12:51:32 +0200

    Add typer to cli.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -8,9 +8,23 @@ import sys
 
 import click
 {%- endif %}
+{%- if cookiecutter.command_line_interface|lower == 'typer' %}
+
+import typer
+
+app = typer.Typer()
+{%- endif %}
 
 {% if cookiecutter.command_line_interface|lower == 'click' %}
 @click.command()
+def main(args=None):
+    """Console script for {{cookiecutter.project_slug}}."""
+    click.echo("Replace this message by putting your code into {{cookiecutter.project_slug}}.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+{%- endif %}
+{%- if cookiecutter.command_line_interface|lower == 'typer' %}
+@app.command()
 def main(args=None):
     """Console script for {{cookiecutter.project_slug}}."""
     click.echo("Replace this message by putting your code into {{cookiecutter.project_slug}}.cli.main")
@@ -32,4 +46,8 @@ def main():
 
 
 if __name__ == "__main__":
+    {%- if cookiecutter.command_line_interface|lower == 'typer' %}
+    sys.exit(app())
+    {%- else %}
     sys.exit(main())  # pragma: no cover
+    {%- endif %}
