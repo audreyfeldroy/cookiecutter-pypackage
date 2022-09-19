5c26681fee36832b444dcfd00cba526dd99a05c8
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2022-09-19 09:57:52 +0200

    clean up tests

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -171,26 +171,6 @@ def test_bake_without_author_file(cookies):
             assert 'AUTHORS.rst' not in manifest_file.read()
 
 
-# def test_bake_without_author_file(cookies):
-#     with bake_in_temp_dir(
-#         cookies,
-#         extra_context={'create_author_file': 'n'}
-#     ) as result:
-#         found_toplevel_files = [f.basename for f in result.project.listdir()]
-#         assert 'AUTHORS.rst' not in found_toplevel_files
-#         doc_files = [f.basename for f in result.project.join('docs').listdir()]
-#         assert 'authors.rst' not in doc_files
-
-#         # Assert there are no spaces in the toc tree
-#         docs_index_path = result.project.join('docs/index.rst')
-#         with open(str(docs_index_path)) as index_file:
-#             assert 'contributing\n   history' in index_file.read()
-
-#         # Check that
-#         manifest_path = result.project.join('MANIFEST.in')
-#         with open(str(manifest_path)) as manifest_file:
-#             assert 'AUTHORS.rst' not in manifest_file.read()
-
 def test_make_help(cookies):
     with bake_in_temp_dir(cookies) as result:
         # The supplied Makefile does not support win32
@@ -231,38 +211,6 @@ def test_bake_not_open_source(cookies):
         assert 'setup.py' in found_toplevel_files
         assert 'LICENSE' not in found_toplevel_files
         assert 'License' not in result.project.join('README.rst').read()
-
-
-# def test_not_using_pytest(cookies):
-#     with bake_in_temp_dir(cookies) as result:
-#         assert result.project.isdir()
-#         test_file_path = result.project.join(
-#             'tests/test_your_python_package.py'
-#         )
-#         lines = test_file_path.readlines()
-#         assert "import unittest" in ''.join(lines)
-#         assert "import pytest" not in ''.join(lines)
-
-
-# def test_project_with_hyphen_in_module_name(cookies):
-#     result = cookies.bake(
-#         extra_context={'project_name': 'something-with-a-dash'}
-#     )
-#     assert result.project is not None
-#     project_path = str(result.project)
-#
-#     # when:
-#     travis_setup_cmd = ('python travis_pypi_setup.py'
-#                         ' --repo audreyr/cookiecutter-pypackage'
-#                         ' --password invalidpass')
-#     run_inside_dir(travis_setup_cmd, project_path)
-#
-#     # then:
-#     result_travis_config = yaml.load(
-#         open(os.path.join(project_path, ".travis.yml"))
-#     )
-#     assert "secure" in result_travis_config["deploy"]["password"],\
-#         "missing password config in .travis.yml"
 
 
 def test_bake_with_no_console_script(cookies):
