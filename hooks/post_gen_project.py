0c9caebd54f454d5fb834875a09a935e53132441
Vince Broz <vince@broz.cc>
2022-10-11 20:11:47 -0400

    Revert syntax

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
         subprocess.check_call(['git', 'add', '-A'])
 
     if (os.environ.get('SKIP_FIX_SCRIPT', '0') != '1' and
-       (os.environ.get('SKIP_GIT_CREATION', '0') != '1')):
+       os.environ.get('SKIP_GIT_CREATION', '0') != '1'):
         subprocess.check_call(['bundle', 'exec', 'overcommit', '--sign'])
         subprocess.check_call(['bundle', 'exec', 'overcommit', '--sign', 'pre-commit'])
         subprocess.check_call(['bundle', 'exec', 'git', 'commit', '-m',
