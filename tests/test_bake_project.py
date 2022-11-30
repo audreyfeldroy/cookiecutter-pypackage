cd5fc03cc0b6b06feb6a4566694868d1ca372d15
Ben Falk <falk.ben@gmail.com>
2022-11-30 09:11:24 -0500

    simplify tests

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -7,7 +7,7 @@ from typing import Generator
 from unittest.mock import patch
 
 from cookiecutter.utils import rmtree
-from pytest_cookies.plugin import Cookies, Result
+from pytest_cookies.plugin import Cookies
 
 
 @contextmanager
@@ -22,22 +22,6 @@ def inside_dir(dirpath: str) -> Generator[None, None, None]:
         yield
     finally:
         os.chdir(old_path)
-
-
-@contextmanager
-def bake_in_temp_dir(
-    cookies: Cookies, *args, **kwargs
-) -> Generator[Result, None, None]:
-    """
-    Delete the temporal directory that is created when executing the tests
-    :param cookies: pytest_cookies.Cookies,
-        cookie to be baked and its temporal files will be removed
-    """
-    result = cookies.bake(*args, **kwargs)
-    try:
-        yield result
-    finally:
-        rmtree(str(result.project_path))
 
 
 def run_inside_dir(command: str, dirpath: str, **kwargs) -> int:
@@ -58,116 +42,112 @@ def check_output_inside_dir(command: str, dirpath: str):
 
 
 @patch.dict(os.environ, {"COOKIECUTTER_SKIP_POST_GEN_HOOK": "1"})
-def test_year_compute_in_license_file(cookies):
-    with bake_in_temp_dir(cookies) as result:
-        assert result.project_path
-        license_file_path = result.project_path / "LICENSE"
-        now = datetime.datetime.now()
-        assert str(now.year) in license_file_path.read_text()
+def test_year_compute_in_license_file(cookies: Cookies):
+    result = cookies.bake()
+    assert result.project_path
+    license_file_path = result.project_path / "LICENSE"
+    now = datetime.datetime.now()
+    assert str(now.year) in license_file_path.read_text()
 
 
 @patch.dict(os.environ, {"COOKIECUTTER_SKIP_POST_GEN_HOOK": "1"})
-def test_bake_with_defaults(cookies):
-    with bake_in_temp_dir(cookies) as result:
-        assert result.project_path
-        assert result.project_path.is_dir()
-        assert result.exit_code == 0
-        assert result.exception is None
+def test_bake_with_defaults(cookies: Cookies):
+    result = cookies.bake()
+    assert result.project_path
+    assert result.project_path.is_dir()
+    assert result.exit_code == 0
+    assert result.exception is None
 
-        found_toplevel_files = [f.name for f in result.project_path.iterdir()]
-        assert "python_boilerplate" in found_toplevel_files
-        assert "pyproject.toml" in found_toplevel_files
-        assert "tests" in found_toplevel_files
+    found_toplevel_files = [f.name for f in result.project_path.iterdir()]
+    assert "python_boilerplate" in found_toplevel_files
+    assert "pyproject.toml" in found_toplevel_files
+    assert "tests" in found_toplevel_files
 
 
 @patch.dict(os.environ, {"COOKIECUTTER_SKIP_POST_GEN_HOOK": "1"})
 def test_bake_and_run_tests(cookies):
-    with bake_in_temp_dir(cookies) as result:
-        assert result.project_path
-        assert result.project_path.is_dir()
-        assert run_inside_dir("pytest", str(result.project_path)) == 0
+    result = cookies.bake()
+    assert result.project_path
+    assert result.project_path.is_dir()
+    assert run_inside_dir("pytest", str(result.project_path)) == 0
 
 
 @patch.dict(os.environ, {"COOKIECUTTER_SKIP_POST_GEN_HOOK": "1"})
-def test_bake_withspecialchars_and_run_tests(cookies):
+def test_bake_withspecialchars_and_run_tests(cookies: Cookies):
     """Ensure that a `full_name` with double quotes does not break"""
-    with bake_in_temp_dir(
-        cookies, extra_context={"full_name": 'name "quote" name'}
-    ) as result:
-        assert result.project_path
-        assert result.project_path.is_dir()
-        assert run_inside_dir("pytest", str(result.project_path)) == 0
+    result = cookies.bake(extra_context={"full_name": 'name "quote" name'})
+    assert result.project_path
+    assert result.project_path.is_dir()
+    assert run_inside_dir("pytest", str(result.project_path)) == 0
 
 
 @patch.dict(os.environ, {"COOKIECUTTER_SKIP_POST_GEN_HOOK": "1"})
-def test_bake_with_apostrophe_and_run_tests(cookies):
+def test_bake_with_apostrophe_and_run_tests(cookies: Cookies):
     """Ensure that a `full_name` with apostrophes does not break"""
-    with bake_in_temp_dir(cookies, extra_context={"full_name": "O'connor"}) as result:
-        assert result.project_path
-        assert result.project_path.is_dir()
-        assert run_inside_dir("pytest", str(result.project_path)) == 0
+    result = cookies.bake(extra_context={"full_name": "O'connor"})
+    assert result.project_path
+    assert result.project_path.is_dir()
+    assert run_inside_dir("pytest", str(result.project_path)) == 0
 
 
 @patch.dict(os.environ, {"COOKIECUTTER_SKIP_POST_GEN_HOOK": "1"})
-def test_bake_and_run_flake8(cookies):
-    with bake_in_temp_dir(cookies) as result:
-        assert result.project_path
-        assert result.project_path.is_dir()
-        assert run_inside_dir("flake8", str(result.project_path)) == 0
+def test_bake_and_run_flake8(cookies: Cookies):
+    result = cookies.bake()
+    assert result.project_path
+    assert result.project_path.is_dir()
+    assert run_inside_dir("flake8", str(result.project_path)) == 0
 
 
 @patch.dict(os.environ, {"COOKIECUTTER_SKIP_POST_GEN_HOOK": "1"})
-def test_bake_and_run_black(cookies):
-    with bake_in_temp_dir(cookies) as result:
-        assert result.project_path
-        assert result.project_path.is_dir()
+def test_bake_and_run_black(cookies: Cookies):
+    result = cookies.bake()
+    assert result.project_path
+    assert result.project_path.is_dir()
 
-        assert result.context
-        project_slug = result.context["project_slug"]
+    assert result.context
+    project_slug = result.context["project_slug"]
 
-        with inside_dir(str(result.project_path)):
-            find_ps = subprocess.Popen(
-                shlex.split(f"find {project_slug}/ -name '*.py' -type f -print"),
-                stdout=subprocess.PIPE,
-            )
-        assert (
-            run_inside_dir(
-                "xargs black --check", str(result.project_path), stdin=find_ps.stdout
-            )
-            == 0
+    with inside_dir(str(result.project_path)):
+        find_ps = subprocess.Popen(
+            shlex.split(f"find {project_slug}/ -name '*.py' -type f -print"),
+            stdout=subprocess.PIPE,
         )
+    assert (
+        run_inside_dir(
+            "xargs black --check", str(result.project_path), stdin=find_ps.stdout
+        )
+        == 0
+    )
 
 
 @patch.dict(os.environ, {"COOKIECUTTER_SKIP_POST_GEN_HOOK": "1"})
-def test_bake_and_run_precommit(cookies):
+def test_bake_and_run_precommit(cookies: Cookies):
 
-    with bake_in_temp_dir(cookies) as result:
-        assert result.project_path
-        assert result.project_path.is_dir()
+    result = cookies.bake()
+    assert result.project_path
+    assert result.project_path.is_dir()
 
-        assert result.context
+    assert result.context
 
-        # install the pre-commit hooks
-        run_inside_dir("pre-commit install-hooks", str(result.project_path))
-        # run against all files
-        assert (
-            run_inside_dir("pre-commit run --all-files", str(result.project_path)) == 0
-        )
+    # install the pre-commit hooks
+    run_inside_dir("pre-commit install-hooks", str(result.project_path))
+    # run against all files
+    assert run_inside_dir("pre-commit run --all-files", str(result.project_path)) == 0
 
 
 @patch.dict(os.environ, {"COOKIECUTTER_SKIP_POST_GEN_HOOK": "0"})
-def test_bake_and_lock_files_created(cookies):
+def test_bake_and_lock_files_created(cookies: Cookies):
 
     # * note: this test doesn't pass within vscode test runner
     # * pip-compile not found on PATH
 
-    with bake_in_temp_dir(cookies) as result:
-        assert result.project_path
-        assert result.project_path.is_dir()
+    result = cookies.bake()
+    assert result.project_path
+    assert result.project_path.is_dir()
 
-        assert result.context
+    assert result.context
 
-        from pathlib import Path
+    from pathlib import Path
 
-        assert Path.exists(Path(result.project_path) / "requirements.txt")
-        assert Path.exists(Path(result.project_path) / "requirements_dev.txt")
+    assert Path.exists(Path(result.project_path) / "requirements.txt")
+    assert Path.exists(Path(result.project_path) / "requirements_dev.txt")
