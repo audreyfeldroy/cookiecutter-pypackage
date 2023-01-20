9a1d2240f66291c1c6d24cbf7d9ca17dfefc7cbd
mazzma12 <mazzma12@protonmail.com>
2023-01-20 12:12:33 +0100

    Increase black version in gihub workflow and add python_version cookiecutter variable

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
@@ -1,10 +1,15 @@
 """Main module."""
 import sys
 
+{%- if cookiecutter.command_line_interface|lower == 'typer' %}
+from .cli import app
 
+app(prog_name="{{cookiecutter.project_slug}}")
+{%- else %}
 def main():
     pass
 
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
+{%- endif %}
