797a73cd73d79e4282cfc5211d4c6fae28a3b38a
Anne Heimes <ahe@akustik.rwth-aachen.de>
2024-02-20 15:41:10 +0100

    add python support for 3.12

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development',
