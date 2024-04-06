755b6b587d6f91c7f8ae452cb0985fb6b9723905
Daniel Roy Greenfeld <62857+pydanny@users.noreply.github.com>
2024-04-06 12:01:59 +0300

    For #706, this replaces click with typer

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/cli.py
@@ -1,23 +1,26 @@
 """Console script for {{cookiecutter.project_slug}}."""
+import {{cookiecutter.project_slug}}
+{%- if cookiecutter.command_line_interface|lower == 'typer' %}
+import typer
 
+app = typer.Typer()
+
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
+
+
+if __name__ == "__main__":
+    app()
+{%- endif %}
 {%- if cookiecutter.command_line_interface|lower == 'argparse' %}
 import argparse
-{%- endif %}
 import sys
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-import click
-{%- endif %}
 
-{% if cookiecutter.command_line_interface|lower == 'click' %}
-@click.command()
-def main(args=None):
-    """Console script for {{cookiecutter.project_slug}}."""
-    click.echo("Replace this message by putting your code into "
-               "{{cookiecutter.project_slug}}.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
-{%- endif %}
-{%- if cookiecutter.command_line_interface|lower == 'argparse' %}
 def main():
     """Console script for {{cookiecutter.project_slug}}."""
     parser = argparse.ArgumentParser()
@@ -28,8 +31,7 @@ def main():
     print("Replace this message by putting your code into "
           "{{cookiecutter.project_slug}}.cli.main")
     return 0
-{%- endif %}
-
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
+{%- endif %}
