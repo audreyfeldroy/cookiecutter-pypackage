a5cf681b64e03a96e0f98d7106bcd7ddda333232
Daniel Roy Greenfeld <62857+pydanny@users.noreply.github.com>
2024-04-06 12:37:42 +0300

    Fine tune CLI component

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/cli.py
@@ -1,37 +1,21 @@
 """Console script for {{cookiecutter.project_slug}}."""
 import {{cookiecutter.project_slug}}
-{%- if cookiecutter.command_line_interface|lower == 'typer' %}
+
 import typer
+from rich.console import Console
 
 app = typer.Typer()
+console = Console()
 
 
 @app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        print(f"Bye {name}!")
+def main():
+    """Console script for {{cookiecutter.project_slug}}."""
+    console.print("Replace this message by putting your code into "
+               "{{cookiecutter.project_slug}}.cli.main")
+    console.print("See Typer documentation at https://typer.tiangolo.com/")
+    
 
 
 if __name__ == "__main__":
     app()
-{%- endif %}
-{%- if cookiecutter.command_line_interface|lower == 'argparse' %}
-import argparse
-import sys
-
-def main():
-    """Console script for {{cookiecutter.project_slug}}."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
-    args = parser.parse_args()
-
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "{{cookiecutter.project_slug}}.cli.main")
-    return 0
-
-if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
-{%- endif %}
