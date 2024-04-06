ef7fe33303d1a2192596a83276ecdb0c0f99af38
franzhaas <franz.dominik.haas@gmail.com>
2024-04-06 11:26:59 +0200

    - using mulitple args instead of / to combine files to paths

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,10 +6,10 @@ if __name__ == '__main__':
 
     if '{{ cookiecutter.create_author_file }}' != 'y':
         pathlib.Path('AUTHORS.rst').unlink()
-        pathlib.Path('docs/authors.rst').unlink()
+        pathlib.Path('docs', 'authors.rst').unlink()
 
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        pathlib.Path('src/{{ cookiecutter.project_slug }}/cli.py').unlink()
+        pathlib.Path('src', '{{ cookiecutter.project_slug }}', 'cli.py').unlink()
         
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         pathlib.Path('LICENSE').unlink()
