1084be2422deb532c8b300a58af9b07bd05abb86
Michael Wagner <michael.wagner.31337@gmail.com>
2025-01-15 14:42:31 +0100

    Remove __main__.py when CLI is set to None

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
 
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
         pathlib.Path('src', '{{ cookiecutter.project_slug }}', 'cli.py').unlink()
-        
+        pathlib.Path('src', '{{ cookiecutter.project_slug }}', '__main__.py').unlink()
+
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         pathlib.Path('LICENSE').unlink()
