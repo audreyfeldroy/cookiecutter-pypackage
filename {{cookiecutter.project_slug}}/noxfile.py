d01e707c7f34d4179ceb82b8596d89a7cdabee2a
Audrey M. Roy Greenfeld <audrey@feldroy.com>
2025-07-18 14:58:32 +0800

    Use nox instead of tox
    Co-authored-by: Daniel Roy Greenfeld <daniel@feldroy.com>

---
+++ {{cookiecutter.project_slug}}/noxfile.py
@@ -0,0 +1,13 @@
+import nox
+
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
+def tests(session):
+    """Run the test suite."""
+    session.install("-r", "requirements_dev.txt")
+    session.run("pytest")
+
+@nox.session
+def ruff(session):
+    """Lint with ruff."""
+    session.install("ruff")
+    session.run("ruff", "check", ".")
