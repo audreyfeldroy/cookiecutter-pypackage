da1782a72bc3f63f0a0b923d3668243597d180ec
Audrey M. Roy Greenfeld <audrey@feldroy.com>
2026-02-16 13:13:29 +0800

    Replace % formatting with f-string in pre_gen hook

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -7,8 +7,8 @@ module_name = "{{ cookiecutter.project_slug}}"
 
 if not re.match(MODULE_REGEX, module_name):
     print(
-        "ERROR: The project slug (%s) is not a valid Python module name. "
-        "Please do not use a - and use _ instead" % module_name
+        f"ERROR: The project slug ({module_name}) is not a valid Python module name. "
+        "Please do not use a - and use _ instead"
     )
     # Exit to cancel project
     sys.exit(1)
